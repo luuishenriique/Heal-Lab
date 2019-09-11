2b4c97b3bab906f4ba7a8451663b8d4b53c2e411
luuishenriique <luis_henrique_co@hotmail.com>
2019-09-11 09:08:22 -0300

    terminando criação e inserção de dados na tabela

--- projetos/proIFPE/Sobre.php
+++ projetos/proIFPE/Sobre.php
@@ -14,61 +14,60 @@
 		<th>Email</th>
 		<th>GitHub</th>
 		<tr>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td>Gabriella Batista</td>
+					<td>gabriella10batista@gmail.com</td>
+					<td>https://github.com/gabriellaBatista</td>
 		</tr>
 		
 
 		<tr>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td>Kauê Luí</td>
+					<td>ofckaue@gmail.com</td>
+					<td>https://github.com/KaueLui</td>
 		</tr>
 
 
 		<tr>
-				<td></td>
-				<td></td>
-				<td></td>
+				<td>Larissa Ferreira</td>
+				<td>larissa.f.lopes@outlook.com</td>
+				<td>https://github.com/FerreiraLarissa</td>
 		</tr>
 
 
 		<tr>
-					<td></td>
-					<td></td>
-					<td></td>
+					<td>Luis Henrique</td>
+					<td>luis_henrique_co@hotmail.com</td>
+					<td>https://github.com/luuishenriique</td>
 		</tr>
 		
 
 		<tr>
-		<td></td>
-		<td></td>
-		<td></td>
+					<td>Maria Conceição</td>
+					<td></td>
+					<td>https://github.com/MARIA617</td>
 		</tr>
 
 
-		<tr><td></td>
-		<td></td>
-		<td></td></tr>
+		<tr>
+					<td>Maria Eduarda</td>
+					<td>mariaeduardaj585@gmail.com</td>
+					<td>https://github.com/Eduarda19</td>
+		</tr>
 
-		<tr><td></td>
-		<td></td>
-		<td></td></tr>
+		<tr>
+					<td>Mateus Lopes</td>
+					<td>mateusyasmim2014@gmail.com</td>
+					<td>https://github.com/Mateuslpds</td>
+		</tr>
 
 
 	</table>
-	<!-- <ul>
-		<li><?php echo "Gabriela Batista" ?></li>
-		<li><?php echo "Kauê Lui" ?></li>
-		<li><?php echo "Larissa Ferreira" ?></li>
-		<li><?php echo "Luis Henrique" ?></li>
-		<li><?php echo "Maria Conceição" ?></li>
-		<li><?php echo "Maria Eduarda" ?></li>
-		<li><?php echo "Mateus Lopes" ?></li>
-	</ul> -->
+
 	<br>
+	<h1>A origem do nome do grupo</h1>
 	<p><?php echo "O nome do grupo se deu a um acrônimo de letras contidas nos nomes dos integrantes do grupo, onde a palavra " ?><strong>Heal</strong> <?php echo " tem como um de seus significados " ?><strong>melhoria</strong><?php echo ". E a segunda a palavra, " ?><strong>Lab</strong><?php echo " signfica " ?><strong>laboratório</strong><?php echo " que leva dois sentidos, um onde é o local onde passamos a maioria de nosso tempo desenvolvendo e o outro é por ser o local de onde sai os experimentos." ?></p>
+	<br>
+	<a href="index.php">Página Inicial</a>
 </body>
 </html>
 </body>
