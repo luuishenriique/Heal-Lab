61069428d83f1e58acde4bfae1e8bfcf9931c826
kauelui <ofckaue@gmail.com>
2019-09-11 08:43:37 -0300

    Adicionado pagina inicial do site

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -23,20 +23,8 @@
 	<br>
 	<p><?php echo "Podendo obter mais funcionalidades durante o processo de desenvolvimento." ?></p>
 	<br>
-	<h2><?php echo "Sobre a equipe de desenvolvimento" ?></h2>
-	<br>
-	<p><?php echo "O grupo de desenvolvimento responsável por desenvolver este projeto tem os seguintes integrantes:" ?></p>
-	<br>
-	<ul>
-		<li><?php echo "Gabriela Batista" ?></li>
-		<li><?php echo "Kauê Lui" ?></li>
-		<li><?php echo "Larissa Ferreira" ?></li>
-		<li><?php echo "Luis Henrique" ?></li>
-		<li><?php echo "Maria Conceição" ?></li>
-		<li><?php echo "Maria Eduarda" ?></li>
-		<li><?php echo "Mateus Lopes" ?></li>
-	</ul>
-	<br>
-	<p><?php echo "O nome do grupo se deu a um acrônimo de letras contidas nos nomes dos integrantes do grupo, onde a palavra " ?><strong>Heal</strong> <?php echo " tem como um de seus significados " ?><strong>melhoria</strong><?php echo ". E a segunda a palavra, " ?><strong>Lab</strong><?php echo " signfica " ?><strong>laboratório</strong><?php echo " que leva dois sentidos, um onde é o local onde passamos a maioria de nosso tempo desenvolvendo e o outro é por ser o local de onde sai os experimentos." ?></p>
+
+	<a rel="stylesheet" href="sobre.php">Sobre</a> 
+	
 </body>
 </html>
