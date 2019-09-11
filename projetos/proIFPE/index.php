4096cbdb8c108393064e0e7233fcf89854b9f74e
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-10 21:09:49 -0300

    adicionando pasta do projeto e index.php para apresentação

---
+++ projetos/proIFPE/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>proIFPE</title>
+</head>
+<body>
+	<h1><?php echo "Projeto de proposta para o ProIFPE"; ?></h1>
+	<br>
+	<h2><?php echo "Informações sobre o projeto"; ?></h2>
+	<p><?php echo "Projeto de alunos do Instituto Federal de Ciência e Tecnologia de Pernambuco - Campus Igarassu" ?></p>
+	<p><?php echo "Cadeira - Projeto e prática I" ?></p>
+	<p><?php echo "Docente - Alexandre Strapação Guedes Vianna" ?></p>
+	<br>
+	<p><?php echo "este projeto está aos encargos de nosso grupo (Heal-Lab) e tem como responsabilidade desenvolver um sistema voltado para o proIFPE, contribuindo para melhorias para esta modalidade de ensino e tentará trazer alguns conceitos de utilização como: "; ?></p>
+	<br>
+	<ul>
+		<li><?php echo "Matrículas de alunos por disciplinas ofertadas" ?></li>
+		<li><?php echo "Controle de presenças dos alunos"; ?></li>
+		<li><?php echo "Registro de aulas ministradas"; ?></li>
+		<li><?php echo "Armarzenamento de avaliações e notas" ?></li>
+	</ul>
+	<br>
+	<p><?php echo "Podendo obter mais funcionalidades durante o processo de desenvolvimento." ?></p>
+	<br>
+	<h2><?php echo "Sobre o desenvolvimento" ?></h2>
+	<br>
+	<p><?php echo "O grupo de desenvolvimento responsável por desenvolver este projeto tem os seguintes integrantes:" ?></p>
+	<br>
+	<ul>
+		<li><?php echo "Gabriela Batista" ?></li>
+		<li><?php echo "Kauê Lui" ?></li>
+		<li><?php echo "Larissa Ferreira" ?></li>
+		<li><?php echo "Luis Henrique" ?></li>
+		<li><?php echo "Maria Conceição" ?></li>
+		<li><?php echo "Maria Eduarda" ?></li>
+		<li><?php echo "Mateus Lopes" ?></li>
+	</ul>
+	<br>
+	<p><?php echo "O nome do grupo se deu a um acrônimo de letras contidas nos nomes dos integrantes do grupo, onde a palavra " ?><strong>Heal</strong> <?php echo " tem como um de seus significados " ?><strong>melhoria</strong><?php echo ". E a segunda a palavra, " ?><strong>Lab</strong><?php echo " signfica " ?><strong>laboratório</strong><?php echo " que leva dois sentidos, um onde é o local onde passamos a maioria de nosso tempo desenvolvendo e o outro é por ser o local de onde sai os experimentos." ?></p>
+</body>
+</html>
