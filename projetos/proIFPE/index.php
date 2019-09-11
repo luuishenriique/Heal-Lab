f4f94784f519844e3a257da43141da0a7db5cff0
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-10 21:49:40 -0300

    alterando título da página do index.php

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>proIFPE</title>
+	<title>proIFPE - Campus Igarassu</title>
 </head>
 <body>
 	<h1><?php echo "Projeto de proposta para o ProIFPE"; ?></h1>
