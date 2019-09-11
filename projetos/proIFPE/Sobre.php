7d28d1da079905d6f48867e89df22cbad18f1605
FerreiraLarissa <laryssaferreira12386@gmail.com>
2019-09-11 08:44:51 -0300

    Adicionando a pagina sobre e iniciando a tabela para nomes

---
+++ projetos/proIFPE/Sobre.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Sobre</title>
+</head>
+<body>
+	<h1><?php echo "Sobre a equipe de desenvolvimento" ?></h1>
+	<br>
+	<p><?php echo "O grupo de desenvolvimento responsável por desenvolver este projeto tem os seguintes integrantes:" ?></p>
+	<br>
+	<table>
+		<th>Nome</th>
+		<th>Email</th>
+		<th>GitHub</th>
+		<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+		</tr>
+		
+
+		<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+		</tr>
+
+
+		<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+		</tr>
+
+
+		<tr>
+					<td></td>
+					<td></td>
+					<td></td>
+		</tr>
+		
+
+		<tr>
+		<td></td>
+		<td></td>
+		<td></td>
+		</tr>
+
+
+		<tr><td></td>
+		<td></td>
+		<td></td></tr>
+
+		<tr><td></td>
+		<td></td>
+		<td></td></tr>
+
+
+	</table>
+	<!-- <ul>
+		<li><?php echo "Gabriela Batista" ?></li>
+		<li><?php echo "Kauê Lui" ?></li>
+		<li><?php echo "Larissa Ferreira" ?></li>
+		<li><?php echo "Luis Henrique" ?></li>
+		<li><?php echo "Maria Conceição" ?></li>
+		<li><?php echo "Maria Eduarda" ?></li>
+		<li><?php echo "Mateus Lopes" ?></li>
+	</ul> -->
+	<br>
+	<p><?php echo "O nome do grupo se deu a um acrônimo de letras contidas nos nomes dos integrantes do grupo, onde a palavra " ?><strong>Heal</strong> <?php echo " tem como um de seus significados " ?><strong>melhoria</strong><?php echo ". E a segunda a palavra, " ?><strong>Lab</strong><?php echo " signfica " ?><strong>laboratório</strong><?php echo " que leva dois sentidos, um onde é o local onde passamos a maioria de nosso tempo desenvolvendo e o outro é por ser o local de onde sai os experimentos." ?></p>
+</body>
+</html>
+</body>
+</html>
