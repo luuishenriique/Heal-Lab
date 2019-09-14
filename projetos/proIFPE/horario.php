200f5009baf464e790c503ddc30e340cccf0a032
gabriellabatista <gabriella10batista@gmail.com>
2019-09-14 13:54:43 -0300

    criando tabela de horario

---
+++ projetos/proIFPE/horario.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Logística</h1>
+	<table>
+		<th>Horário</th>
+		<th>Segunda</th>
+		<tr>
+			<td>09:15/10:00</td>
+			<td>Matemática (Cicero)</td>
+		</tr>
+	</table>
+	<table>
+		<th>Horário</th>
+		<th>Segunda</th>
+		<tr>
+			<td>14:20/15:05</td>
+			<td>Matemática (Maurício)</td>
+		</tr>
+	</table>
+	<h1>Informática para Internet</h1>
+	<table>
+		<th>Horário</th>
+		<th>Terça</th>
+		<th>Sexta</th>
+		<tr>
+			<td>12:50/13:25</td>
+			<td>Informática (Ranieri)</td>
+		</tr>
+		<tr>
+			<td>14:20/15:05</td>
+			<td>Matemática (Maurício)</td>
+		</tr>
+		<tr>
+			<td>15:25/16:10</td>
+			<td>Matemática (Maurício)</td>
+		</tr>
+	</table>
+	<table>
+		<th>Horário</th>
+		<th>Segunda</th>
+		<tr>
+			<td>14:20/15:05</td>
+			<td>Matemática (Maurício)</td>
+		</tr>
+	</table>
+</body>
+</html>
