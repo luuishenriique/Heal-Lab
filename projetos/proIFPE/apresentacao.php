8df80f4141f305fd1a1b52ca74da0ccb388c885b
Mateuslpds <mateusyasmim2014@gmail.com>
2019-09-15 15:26:41 -0300

    alterando pagina de apresentação

--- projetos/proIFPE/apresentacao.php
+++ projetos/proIFPE/apresentacao.php
@@ -6,6 +6,22 @@
 </head>
 <body>
 	
+	<div class="header">
+	<a href="index.php"><img src="logo.png" alt="logo" class="logo"></a>	
+	<i class="Arial"></i>
+	</label>
+
+	<ul class="menu">
+		<a href="index.php">Início</a>
+		<a href="Sobre.php">Sobre</a>
+		<a href="horario.php">Horário</a>
+		<i class="Arial"></i>
+		</label>
+	</ul>
+	</div>
+
+	<h1><?php echo "Informações sobre as cadeiras e docentes participates do ProIFPE"; ?></h1>
+	<br>
 	
 
 </body>
