dbc5053293e599be8dea75d72059020236aa0455
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-21 22:29:03 -0300

    ajustando identação e removendo php echos desnecessários

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -9,39 +9,38 @@
 </head>
 <body>
 	<div class="header">
-    <a href="index.php"><img src="logo.png" alt="logo" class="logo">
-      <i class="Arial"></i>
-    </label>
-
-    <ul class="menu">
-      <a href="sobre.php">Sobre</a>
-       <a href="horario.php">Horário</a>
-       <a href="disciplinas.php">Professores</a>
-        <i class="Arial"></i>
+    	<a href="index.php"><img src="logo.png" alt="logo" class="logo">
+      	<i class="Arial"></i>
+   		</label>
+    		<ul class="menu">
+      		<a href="Sobre.php">Sobre</a>
+       		<a href="horario.php">Horário</a>
+       		<a href="disciplinas.php">Professores</a>
+        	<i class="Arial"></i>
+    		</ul>
       </label>
-    </ul>
   </div>
 
-	<h1><?php echo "Projeto de proposta para o ProIFPE"; ?></h1>
+	<h1>Projeto de proposta para o ProIFPE</h1>
 	<br>
-	<h2><?php echo "Informações sobre o projeto"; ?></h2>
-	<p><?php echo "Projeto de alunos do Instituto Federal de Ciência e Tecnologia de Pernambuco - Campus Igarassu" ?></p>
-	<p><?php echo "Cadeira - Projeto e prática I" ?></p>
-	<p><?php echo "Docente - Alexandre Strapação Guedes Vianna" ?></p>
-	<p><?php echo "Professor Idealizador - Francisco Chaves Pinto" ?></p>
+	<h2>Informações sobre o projeto</h2>
+	<p>Projeto de alunos do Instituto Federal de Ciência e Tecnologia de Pernambuco - Campus Igarassu</p>
+	<p>Cadeira - Projeto e prática I</p>
+	<p>Docente - Alexandre Strapação Guedes Vianna</p>
+	<p>Professor Idealizador - Francisco Chaves Pinto</p>
 	<br>
-	<p><?php echo "Este projeto está aos encargos de nosso grupo (Heal-Lab) e tem como responsabilidade desenvolver um sistema voltado para o proIFPE, contribuindo para melhorias para esta modalidade de ensino e tentará trazer alguns conceitos de utilização como: "; ?></p>
+	<p>Este projeto está aos encargos de nosso grupo (Heal-Lab) e tem como responsabilidade desenvolver um sistema voltado para o proIFPE, contribuindo para melhorias para esta modalidade de ensino e tentará trazer alguns conceitos de utilização como:</p>
 	<br>
 	<ul>
-		<li><?php echo "Matrículas de alunos por disciplinas ofertadas" ?></li>
-		<li><?php echo "Controle de presenças dos alunos"; ?></li>
-		<li><?php echo "Registro de aulas ministradas"; ?></li>
-		<li><?php echo "Armarzenamento de avaliações e notas" ?></li>
+		<li>Matrículas de alunos por disciplinas ofertadas</li>
+		<li>Controle de presenças dos alunos</li>
+		<li>Registro de aulas ministradas</li>
+		<li>Armarzenamento de avaliações e notas</li>
 	</ul>
 	<br>
-	<p><?php echo "Podendo obter mais funcionalidades durante o processo de desenvolvimento." ?></p>
-	<p><?php echo "O projeto tem como objetivo principal, criar um site que compute as faltas e notas de todos os alunos do PROIFPE." ?></p>
-	<p><?php echo "A nossa expectativa é que esse site funcione em nossa instituição." ?></p>
+	<p>Podendo obter mais funcionalidades durante o processo de desenvolvimento.</p>
+	<p>O projeto tem como objetivo principal, criar um site que compute as faltas e notas de todos os alunos do PROIFPE.</p>
+	<p>A nossa expectativa é que esse site funcione em nossa instituição.</p>
 	<br>
 
 	
