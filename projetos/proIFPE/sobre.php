3f195a1bb2270bfc35046b226a6bfa3361af6be7
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-24 22:52:11 -0300

    alterando referência do arquivo .css

---
+++ projetos/proIFPE/sobre.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<title>Sobre</title>
+</head>
+<body>
+	<?php include "header.php" ?>
+	<h1>Sobre a equipe de desenvolvimento</h1>
+	<br>
+	<p>O grupo de desenvolvimento responsável por desenvolver este projeto tem os seguintes integrantes:</p>
+	<br>
+	<table>
+		<th>Nome</th>
+		<th>Email</th>
+		<th>GitHub</th>
+		<tr>
+					<td>Gabriella Batista</td>
+					<td>gabriella10batista@gmail.com</td>
+					<td><a href="https://github.com/gabriellaBatista">https://github.com/gabriellaBatista</a></td>
+		</tr>
+		
+
+		<tr>
+					<td>Kauê Luí</td>
+					<td>ofckaue@gmail.com</td>
+					<td><a href="https://github.com/KaueLui">https://github.com/KaueLui</a></td>
+		</tr>
+
+
+		<tr>
+				    <td>Larissa Ferreira</td>
+				    <td>larissa.f.lopes@outlook.com</td>
+				    <td><a href="https://github.com/FerreiraLarissa">https://github.com/FerreiraLarissa</a></td>
+		</tr>
+
+
+		<tr>
+					<td>Luis Henrique</td>
+					<td>luis_henrique_co@hotmail.com</td>
+					<td><a href="https://github.com/luuishenriique">https://github.com/luuishenriique</a></td>
+		</tr>
+		
+
+		<tr>
+					<td>Maria Conceição</td>
+					<td>maria4682@gmail.com</td>
+					<td><a href="https://github.com/MARIA617">https://github.com/MARIA617</a></td>
+		</tr>
+
+
+		<tr>
+					<td>Maria Eduarda</td>
+					<td>mariaeduardaj585@gmail.com</td>
+					<td><a href="https://github.com/Eduarda19">https://github.com/Eduarda19</a></td>
+		</tr>
+
+		<tr>
+					<td>Mateus Lopes</td>
+					<td>mateusyasmim2014@gmail.com</td>
+					<td><a href="https://github.com/Mateuslpds">https://github.com/Mateuslpds</a></td>
+		</tr>
+
+
+	</table>
+
+	<br>
+	<h1>A origem do nome do grupo</h1>
+	<p>O nome do grupo se deu a um acrônimo de letras contidas nos nomes dos integrantes do grupo, onde a palavra <strong>Heal</strong> tem como um de seus significados <strong>melhoria</strong>. E a segunda a palavra, <strong>Lab</strong>, signfica <strong>laboratório</strong>, que leva dois sentidos, um onde é o local onde passamos a maioria de nosso tempo desenvolvendo e o outro é por ser o local de onde sai os experimentos.</p>
+	<br>
+	
+</body>
+</html>
+</body>
+</html>
