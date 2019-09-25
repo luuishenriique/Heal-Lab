9b3b81ad059efb0e3c97be28c9166dfd5ce44d02
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-24 22:49:57 -0300

    trocando informações de link no header.php

--- projetos/proIFPE/header.php
+++ projetos/proIFPE/header.php
@@ -1,12 +1,12 @@
 <div class="header">
-    <a href="index.php"><img src="logo.png" alt="logo" class="logo">
+    <a href="index.php"><img src="img/logo.png" alt="logo" class="logo">
     <i class="Arial"></i>
 	   	</label>
 	    	<ul class="menu">
 	       	<a href="index.php">Início</a>
 	       	<a href="disciplinas.php">Professores</a>
 	       	<a href="horario.php">Horário</a>
-	      	<a href="Sobre.php">Sobre</a>
+	      	<a href="sobre.php">Sobre</a>
 	    	</ul>
 	    </label>
     <i class="Arial"></i>
