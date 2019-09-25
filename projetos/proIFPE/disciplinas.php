acd2dd751839f26b9384f0e98fd3082dfe011cd1
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-24 22:48:31 -0300

    inserindo links de inserção de professores e disciplinas

--- projetos/proIFPE/disciplinas.php
+++ projetos/proIFPE/disciplinas.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Disciplinas</title>
-  <link rel="stylesheet" href="estilocards.css">
+  <link rel="stylesheet" href="css/estilocards.css">
 </head>
 <body>
 <?php include "header.php" ?>
@@ -41,6 +41,16 @@
   <p>Matéria ministrada pelo docente Ranieri e apenas para os alunos do curso de Informática para Internet do 1º período, nela é ensinado o básico de HTML e CSS, além de serem respondidas algumas dúvidas sobre JavaScript</p>
   <h2>ProIFPE Matemática</h2>
   <p>Matéria ministrada pelo docente Cicero para alunos de Logística do 1º Período, e pelo professor Maurício, que ensina os alunos dos cursos de Logística e Informática para Internet do 1º período à tarde, na matéria são reforçados assuntos dados nas aulas normais</p>-->
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<a href="cadastro_professores.php" style="text-align: center;">Adicionar Professores</a>
+<a href="cadastro_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a>
 
 </body>
 </html>
