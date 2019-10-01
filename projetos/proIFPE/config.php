6b428c0370834ef8eb57ca34ecbf3bcec17947ee
Maria Eduarda <mariaeduardaj585@gmail.com>
2019-10-01 16:30:58 -0300

    Corrigindo erro do config

--- projetos/proIFPE/config.php
+++ projetos/proIFPE/config.php
@@ -1,6 +1,6 @@
 <?php 
-define(PRO_FILE, 'src/professores.csv');
-define(DIS_FILE, 'src/disciplinas.csv');
+define('PRO_FILE', 'src/professores.csv');
+define('DIS_FILE', 'src/disciplinas.csv');
 
 function redicect($url){
 	header('Location: ' . $url);
