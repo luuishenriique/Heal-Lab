d0b68f952c6cee05bce5c1bc614a55ae1fafd5c4
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-01 18:49:30 -0300

    adicionando constante de arquivo csv para alunos

--- projetos/proIFPE/config.php
+++ projetos/proIFPE/config.php
@@ -1,6 +1,7 @@
 <?php 
 define('PRO_FILE', 'src/professores.csv');
 define('DIS_FILE', 'src/disciplinas.csv');
+define('ALN_FILE', 'src/alunos.csv');
 
 function redicect($url){
 	header('Location: ' . $url);
