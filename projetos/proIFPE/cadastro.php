9a23255d568b4e58c21c799fc4ef8bd44c71bbe6
Maria Eduarda <mariaeduardaj585@gmail.com>
2019-10-01 16:47:48 -0300

    Criando página para cadastro

---
+++ projetos/proIFPE/cadastro.php
@@ -0,0 +1,10 @@
+<title>Cadastro</title>
+
+<?php 
+include "header.php";
+include "config.php";
+?>
+
+
+<a href="cadastro_professores.php" style="text-align: center;">Adicionar Professores</a>
+  <a href="cadastro_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a>
