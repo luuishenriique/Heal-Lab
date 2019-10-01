9744ef730e54f3e842840a79788089376ae20048
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-30 23:10:03 -0300

    inserindo estrutura condicional para aparição de get com mensagem informativa e ajustando indentação

--- projetos/proIFPE/cadastro_professores.php
+++ projetos/proIFPE/cadastro_professores.php
@@ -1,41 +1,48 @@
-	<title>Cadastro de Professores</title>
+<title>Cadastro de Professores</title>
 
-	<?php 
-		include 'header.php';
+<?php 
+include 'header.php';
+include 'config.php';
 
-		$dados = file('src/professores.csv'); /*recebendo arquivos da tabela csv*/
-      	for ($i=0; $i < sizeof($dados); $i++) { 
-        $dados[$i] = explode(',', $dados[$i]);
-      }
-	 ?>
-	 
-	 <legend>Informações sobre o professor</legend>
-	 <form action="add_prof.php" method="POST" style="text-align: center;">
-	 	<fieldset>
-	 		<input type="text" name="professor" placeholder="Professor"><br>
-	 		<input type="text" name="disciplina" placeholder="Disciplina"><br>
-	 		<input type="text" name="email" placeholder="Email"><br>
-	 		<input type="submit" value="Adicionar">
-	 	</fieldset>
-	 </form>
+$dados = file(PRO_FILE); /*recebendo arquivos da tabela csv*/
+for ($i=0; $i < sizeof($dados); $i++) { 
+	$dados[$i] = explode(',', $dados[$i]);
+}
+?>
 
-	 <table>
-	 	<tr>
-	 		<th>Professor</th>
-	 		<th>Disciplina</th>
-	 		<th>Email</th>
-	 		<th>Apagar Professor</th>
-	 	</tr>
-	 	<?php foreach ($dados as $i => $dado): ?>
-	 		<tr>
-	 			<?php foreach ($dado as $dados): ?>
-	 				<td><?= $dados ?></td>
-	 			<?php endforeach ?>
-	 				<td>
-	 					<a href="del_prof?linha=<?= $i ?>">X</a>
-	 				</td>
-	 		</tr>
-	 	<?php endforeach ?>
-	 </table>
+<?php if (!empty($_GET['msg'])): ?>
+	<div class="msg_info">
+		<h4><?= $_GET['msg'] ?></h4>
+	</div>
+<?php endif ?>
+
+<legend>Informações sobre o professor</legend>
+<form action="add_prof.php" method="POST" style="text-align: center;">
+	<fieldset>
+		<input type="text" name="professor" placeholder="Professor"><br>
+		<input type="text" name="disciplina" placeholder="Disciplina"><br>
+		<input type="text" name="email" placeholder="Email"><br>
+		<input type="submit" value="Adicionar">
+	</fieldset>
+</form>
+
+<table>
+	<tr>
+		<th>Professor</th>
+		<th>Disciplina</th>
+		<th>Email</th>
+		<th>Apagar Professor</th>
+	</tr>
+	<?php foreach ($dados as $i => $dado): ?>
+		<tr>
+			<?php foreach ($dado as $dados): ?>
+				<td><?= $dados ?></td>
+			<?php endforeach ?>
+			<td>
+				<a href="del_prof.php?linha=<?= $i ?>">X</a>
+			</td>
+		</tr>
+	<?php endforeach ?>
+</table>
 </body>
 </html>
