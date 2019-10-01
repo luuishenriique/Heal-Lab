b420f6810f0ea4cd77bf53d94e09a2e8b00e451a
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-30 23:14:57 -0300

    ajustando indentação da página

--- projetos/proIFPE/header.php
+++ projetos/proIFPE/header.php
@@ -9,21 +9,20 @@
 <body>
 
 
-<div class="header">
-    <span id="nome">IFPE</span>
-    <h1 class="logo">Instituto Federal de Pernambuco</h1>
+	<div class="header">
+		<span id="nome">IFPE</span>
+		<h1 class="logo">Instituto Federal de Pernambuco</h1>
+	</div>
+	<div id="sobre">
+	</label>
+	<ul class="menu">
+		<a href="index.php">Início</a>
+		<a href="disciplinas.php">Professores</a>
+		<a href="horario.php">Horário</a>
+		<a href="sobre.php">Sobre</a>
+	</ul>
+</label>
+
 </div>
-    <div id="sobre">
-	   	</label>
-	    	<ul class="menu">
-	       	<a href="index.php">Início</a>
-	       	<a href="disciplinas.php">Professores</a>
-	       	<a href="horario.php">Horário</a>
-	      	<a href="sobre.php">Sobre</a>
-	    	</ul>
-	    </label>
-   
-   </div>
 
 
- 
