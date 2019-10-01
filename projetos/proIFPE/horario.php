c923c7fa4492abdb7720d7440bdcc932099742ef
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-30 23:15:56 -0300

    ajustando indentação do arquivo

--- projetos/proIFPE/horario.php
+++ projetos/proIFPE/horario.php
@@ -1,69 +1,69 @@
 <title>Horário</title>
 
-	<?php include "header.php" ?>
-  	<h1>Horários</h1>
-  	<p>Tabelas contendo horários e dias das disciplinas paricipantes do ProIFPE</p>
-	<br>
-	<h1><u>Logística</u></h1>
-	<br>
-	<h2>Manhã</h2>
-	<br>
-	<table>
-		<tr>
-			<th>Horário</th>
-			<th>Segunda-Feira</th>
-			
-		</tr>
-		<tr>
-			<td>09:15 ~ 10:00</td>
-			<td>Matemática (Prof° Cicero)</td>
-		</tr>
+<?php include "header.php"; ?>
+<h1>Horários</h1>
+<p>Tabelas contendo horários e dias das disciplinas paricipantes do ProIFPE</p>
+<br>
+<h1><u>Logística</u></h1>
+<br>
+<h2>Manhã</h2>
+<br>
+<table>
+	<tr>
+		<th>Horário</th>
+		<th>Segunda-Feira</th>
+
+	</tr>
+	<tr>
+		<td>09:15 ~ 10:00</td>
+		<td>Matemática (Prof° Cicero)</td>
+	</tr>
 </table>
-			<h2>Tarde</h2>
-				<br>
-				<table>
-					<tr>
-						<th>Horário</th>
-						<th>Segunda-Feira</th>
-						
-					</tr>
-					<tr>
-						<td>14:20 ~ 15:05</td>
-						<td>Matemática (Prof° Maurício)</td>
-					</tr>
-			</table>
+<h2>Tarde</h2>
+<br>
+<table>
+	<tr>
+		<th>Horário</th>
+		<th>Segunda-Feira</th>
+
+	</tr>
+	<tr>
+		<td>14:20 ~ 15:05</td>
+		<td>Matemática (Prof° Maurício)</td>
+	</tr>
+</table>
 
 
 
-	<br>
-		<h1><u>Informática</u></h1>
-		<br>
-		<h2>Tarde</h2>
-		<br>
-		<table>
-			<tr>
-				<th>Horário</th>
-				<th>Terça-Feira</th>
-				<th>Sexta-Feira</th>
-			</tr>
-			<tr>
-				<td>12:50 ~ 13:35</td>
-				<td>Informática (Prof° Ranieri)</td>
-				<td></td>
-			</tr>
-			<tr>
-				<td>14:20 ~ 15:05</td>
-				<td></td>
-				<td>Matemática (Prof° Maurício)</td>
-			</tr>
-			<tr>
-				<td>15:25 ~ 16:10</td>
-				<td></td>
-				<td>Matemática (Prof° Maurício)</td>
-			</tr>
-	</table>
-	<br>
-	<p>Todos horários citados acima representam as cadeiras e professores que participam do programa.</p>
-	<br>
+<br>
+<h1><u>Informática</u></h1>
+<br>
+<h2>Tarde</h2>
+<br>
+<table>
+	<tr>
+		<th>Horário</th>
+		<th>Terça-Feira</th>
+		<th>Sexta-Feira</th>
+	</tr>
+	<tr>
+		<td>12:50 ~ 13:35</td>
+		<td>Informática (Prof° Ranieri)</td>
+		<td></td>
+	</tr>
+	<tr>
+		<td>14:20 ~ 15:05</td>
+		<td></td>
+		<td>Matemática (Prof° Maurício)</td>
+	</tr>
+	<tr>
+		<td>15:25 ~ 16:10</td>
+		<td></td>
+		<td>Matemática (Prof° Maurício)</td>
+	</tr>
+</table>
+<br>
+<p>Todos horários citados acima representam as cadeiras e professores que participam do programa.</p>
+<br>
 </body>
 </html>
