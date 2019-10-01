9c1be30e87dd0584e4d3f819908fde064050015d
Gabriella Batista <gabriella10batista@gmail.com>
2019-10-01 11:49:28 -0300

    Adicionando icon

--- projetos/proIFPE/header.php
+++ projetos/proIFPE/header.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" href="css/estilo.css">
 	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" type="image/x-icon" href="img/5235fav.ico">
 </head>
 <body>
 
@@ -12,6 +13,7 @@
 	<div class="header">
 		<span id="nome">IFPE</span>
 		<h1 class="logo">Instituto Federal de Pernambuco</h1>
+		<span id="nome-1">ProIFPE - Campus Igarassu</span>
 	</div>
 	<div id="sobre">
 	</label>
