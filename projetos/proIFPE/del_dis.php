4c58230f74e2542ca72e2caa394c280501d9cebc
Luis Henrique <luis_henrique_co@hotmail.com>
2019-09-30 23:12:34 -0300

    incluindo arquivo config.php e adaptando arquivo para a mesma

--- projetos/proIFPE/del_dis.php
+++ projetos/proIFPE/del_dis.php
@@ -1,13 +1,15 @@
 <?php
-	$linha = $_GET['linha']; /*get para receber o valor da linha*/
-	$info = file('src/disciplinas.csv');
-	unset($info[$linha]);
-	$str = ""; /*dados vazios que vão limpar a linha*/
-	foreach ($info as $dados) {
-		$str = $str.$dados;
-	}
-	$file = fopen('src/disciplinas.csv', "w");
-	fwrite($file, $str); /*dados sendo transcitos pela linha vazia*/
+include 'config.php';
 
-	header('location:cadastro_disciplinas.php');
+$linha = $_GET['linha']; /*get para receber o valor da linha*/
+$info = file(DIS_FILE);
+unset($info[$linha]);
+$str = ""; /*dados vazios que vão limpar a linha*/
+foreach ($info as $dados) {
+	$str = $str.$dados;
+}
+$file = fopen(DIS_FILE, "w");
+fwrite($file, $str); /*dados sendo transcitos pela linha vazia*/
+
+redicect("cadastro_disciplinas.php?msg=Registro apagado com sucesso!");
 ?>
