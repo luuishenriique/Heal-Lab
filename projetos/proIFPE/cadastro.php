c56b7b0fea9686970ad8732e6c6582b3570df1de
MARIA617 <mariac4682@gmail.com>
2019-10-01 18:46:00 -0300

    adicionando mais links em página de cadastro

--- projetos/proIFPE/cadastro.php
+++ projetos/proIFPE/cadastro.php
@@ -6,5 +6,6 @@ include "config.php";
 ?>
 
 
-<a href="cadastro_professores.php" style="text-align: center;">Adicionar Professores</a>
-  <a href="cadastro_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a>
+<h2><a href="cad_professores.php" style="text-align: center;">Adicionar Professores</a></h2>
+<h2><a href="cad_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a></h2>
+<h2><a href="cad_alunos.php" style="text-align: center;">Adicionar Alunos</a></h2>
