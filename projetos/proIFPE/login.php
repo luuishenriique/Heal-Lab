2d6403fdc994616bdef30537e0388ed110a81ec3
Kaue Lui <ofckaue@gmail.com>
2019-10-14 17:55:58 -0300

    Adicionando pagina de login

---
+++ projetos/proIFPE/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" type="image/x-icon" href="img/5235fav.ico">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+</head>
+<body>
+
+
+	<div class="header">
+		<span id="nome">IFPE</span>
+		<h1 class="logo">Instituto Federal de Pernambuco</h1>
+		<span id="nome-1">ProIFPE - Campus Igarassu</span>
+	</div>
+	<div id="sobre">
+	</label>
+	<ul class="menu">
+		
+	</ul>
+</label>
+
+</div>
+
+	<h2><a href="cadastro.php" style="text-align: center;">CADASTRO</a></h2>
+    <h2><a href="#" style="text-align: center;">LOGIN</a></h2>
+	
