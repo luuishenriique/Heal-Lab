0c3465536fe668f84fb66a96a709f72502b6ce7d
Mateus Lopes <mateusyasmim2014@gmail.com>
2019-10-14 19:18:45 -0300

    Adicionando opções de cadastro e login no index

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -3,5 +3,8 @@
 	<?php include "header.php" ?>
 	<h1>ProIFPE - Campus Igarassu</h1>
 	<p>Sistema de dados para o ProIFPE</p>
+
+	<h2><a href="cadastro.php" style="text-align: center;">CADASTRO</a></h2>
+    <h2><a href="#" style="text-align: center;">LOGIN</a></h2>
 </body>
 </html>
