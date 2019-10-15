286df937701482dcd6afde42d9f5fc21fd841132
Larissa Ferreira <laryssaferreira12386@gmail.com>
2019-10-15 18:38:28 -0300

    Modificando add de prof

--- projetos/proIFPE/add_prof.php
+++ projetos/proIFPE/add_prof.php
@@ -1,9 +1,19 @@
 <?php
 include 'config.php';
 
-$nome = $_POST['professor']. ',' .  $_POST['disciplina']. ',' . $_POST['email']. "\n";
-$dados = fopen(PRO_FILE, 'a');
-fwrite($dados,$nome);
+$nome = $_POST['professor'];
+$disciplina = $_POST['select-dis']; 
+$dis_dt = $disciplina[0];
+$email = $_POST['email'];
+
+if ($nome == false || $email == false){
+	redicect("cad_professores.php?msg=Os campos precisam ser preenchidos");
+}
+
+$dados = join(",",[$nome,$dis_dt, $email]) . "\n";
+$handle = fopen(PRO_FILE, 'a');
+fwrite($handle,$dados);
+fclose($handle);
 
 redicect("cad_professores.php?msg=Registro inserido com sucesso!");
 ?>
