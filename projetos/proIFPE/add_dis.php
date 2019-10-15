232c1a72c807a5669ab168cea2514946882ab802
Larissa Ferreira <laryssaferreira12386@gmail.com>
2019-10-15 18:39:22 -0300

    Modificando add de disciplina

--- projetos/proIFPE/add_dis.php
+++ projetos/proIFPE/add_dis.php
@@ -1,9 +1,15 @@
 <?php 
 include 'config.php';
 
-$nome = $_POST['disciplina']. ',' .  $_POST['dia']. ',' . $_POST['horario']. ',' . $_POST['turno']. ',' . $_POST['inicio']. ',' . $_POST['termino']. "\n";
+$nome = $_POST['disciplina']. ',' .  $_POST['dia']. ',' . $_POST['horario']. ',' . $_POST['inicio']. ',' . $_POST['termino']. "\n";
+
+if($nome == false){
+	redicect("cad_disciplinas.php?msg=Os campos precisam ser preenchidos!");
+}
+
 $dados = fopen(DIS_FILE, 'a');
 fwrite($dados,$nome);
+fclose($dados);
 
 redicect("cad_disciplinas.php?msg=Registro inserido com sucesso!");
 ?>
