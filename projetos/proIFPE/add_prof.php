c2bff30155944e8ba71a501ab9c903485d6b0bbd
Larissa Ferreira <laryssaferreira12386@gmail.com>
2019-10-15 18:55:10 -0300

    Corrigindo um erro de add de professor

--- projetos/proIFPE/add_prof.php
+++ projetos/proIFPE/add_prof.php
@@ -3,14 +3,13 @@ include 'config.php';
 
 $nome = $_POST['professor'];
 $disciplina = $_POST['select-dis']; 
-$dis_dt = $disciplina[0];
 $email = $_POST['email'];
 
 if ($nome == false || $email == false){
 	redicect("cad_professores.php?msg=Os campos precisam ser preenchidos");
 }
 
-$dados = join(",",[$nome,$dis_dt, $email]) . "\n";
+$dados = join(",",[$nome,$disciplina,$email]) . "\n";
 $handle = fopen(PRO_FILE, 'a');
 fwrite($handle,$dados);
 fclose($handle);
