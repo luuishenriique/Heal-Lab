b6339a2f4a70fef9f4ffe8581a6bba6412fa51fe
Gabriella Batista <gabriella10batista@gmail.com>
2019-10-15 22:08:36 -0300

    adicionando variáves de captura de dados e ajustando condições de inserção

--- projetos/proIFPE/add_prof.php
+++ projetos/proIFPE/add_prof.php
@@ -1,15 +1,17 @@
 <?php
 include 'config.php';
 
+$siape = $_POST['siape'];
 $nome = $_POST['professor'];
-$disciplina = $_POST['select-dis']; 
+$disciplina = $_POST['disc-prof']; 
 $email = $_POST['email'];
 
-if ($nome == false || $email == false){
+if ($siape == false || $nome == false || $email == false || $disciplina == ""){
 	redicect("cad_professores.php?msg=Os campos precisam ser preenchidos");
+	exit();
 }
 
-$dados = join(",",[$nome,$disciplina,$email]) . "\n";
+$dados = join(",",[$siape,$nome,$disciplina, $email]) . "\n";
 $handle = fopen(PRO_FILE, 'a');
 fwrite($handle,$dados);
 fclose($handle);
