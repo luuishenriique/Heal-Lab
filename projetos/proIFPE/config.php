825b75db34d64928df4dce2a220b71de1a3cf123
Eduarda19 <mariaeduardaj585@gmail.com>
2019-10-15 23:34:21 -0300

    inserindo constante do arquivo de notificacao.csv com sua referência

--- projetos/proIFPE/config.php
+++ projetos/proIFPE/config.php
@@ -2,6 +2,7 @@
 define('PRO_FILE', 'src/professores.csv');
 define('DIS_FILE', 'src/disciplinas.csv');
 define('ALN_FILE', 'src/alunos.csv');
+define('NOT_FILE', 'src/notificacao.csv');
 
 function redicect($url){
 	header('Location: ' . $url);
