5022177450d383806c5ef05ea90f6c1db7768cee
Gabriella Batista <gabriella10batista@gmail.com>
2019-10-15 22:07:46 -0300

    adicionando variáves de captura de dados e ajustando condições de inserção

--- projetos/proIFPE/add_dis.php
+++ projetos/proIFPE/add_dis.php
@@ -1,15 +1,22 @@
 <?php 
 include 'config.php';
 
-$nome = $_POST['disciplina']. ',' .  $_POST['dia']. ',' . $_POST['horario']. ',' . $_POST['inicio']. ',' . $_POST['termino']. "\n";
+$disciplina = $_POST['disciplina'];
+$dia = $_POST['dia'];
+$turno = $_POST['turno'];
+$inicio = $_POST['inicio'];
+$termino = $_POST['termino'];
 
-if($nome == false){
-	redicect("cad_disciplinas.php?msg=Os campos precisam ser preenchidos!");
+
+$cont = $_POST['disciplina']. ',' .  $_POST['dia']. ',' . $_POST['turno']. ',' . $_POST['inicio']. ',' . $_POST['termino']. "\n";
+
+if($disciplina == "" || $dia == "" || $turno == "" || $inicio == false || $termino == false){
+	redicect("cad_disciplinas.php?msg=Todos os campos precisam ser preenchidos!");
+} else {
+	$dados = fopen(DIS_FILE, 'a');
+	fwrite($dados,$cont);
+	fclose($dados);
+
+	redicect("cad_disciplinas.php?msg=Registro inserido com sucesso!");	
 }
-
-$dados = fopen(DIS_FILE, 'a');
-fwrite($dados,$nome);
-fclose($dados);
-
-redicect("cad_disciplinas.php?msg=Registro inserido com sucesso!");
 ?>
