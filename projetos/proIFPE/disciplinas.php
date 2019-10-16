bd859dd5af376a061a64ffe9a1bfe11dca95fff5
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-15 22:20:37 -0300

    ajustando cards de apresentação das disciplinas

--- projetos/proIFPE/disciplinas.php
+++ projetos/proIFPE/disciplinas.php
@@ -1,50 +1,18 @@
 <title>Professores</title>
 
 <?php include "header.php" ?>
-<div class="container">
-  <div class="card">
-   <div class="imgBx">
-    <div>
-      <img src="../img/calculator.png">
-      <h2>Matemática</h2>
-    </div>
-  </div>
-  <div class="overlay"></div>
-  <div class="content">
-    <h2>Matemática</h2>
-    <p>Matéria ministrada pelo docente Cicero para alunos de Logística do 1º Período, e pelo professor Maurício, que ensina os alunos dos cursos de Logística e Informática para Internet do 1º período à tarde, na matéria são reforçados assuntos dados nas aulas normais.</p>
-  </div>
+
+<br>
+<div class ="card_mat">
+	<img class="img_mat" src="img/calculator.png" alt="Calculadora" style="width: 25%">
+	<p class="title_mat"><b>Matemática</b></p>
+	<p class="p_mat">Matéria ministrada pelo docente Cicero para alunos de Logística do 1º Período, e pelo professor Maurício, que ensina os alunos dos cursos de Logística e Informática para Internet do 1º período à tarde, na matéria são reforçados assuntos dados nas aulas normais.</p> 
+</div>
+<br>
+<div class="card_mat">
+	<img class="img_mat" src="img/monitor.png" alt="Monitor" style="width: 25%">
+	<p class="title_mat"><b>Informática</b></p>
+	<p class="p_mat">Matéria ministrada pelo docente Ranieri e apenas para os alunos do curso de Informática para Internet do 1º período, nela é ensinado o básico de HTML e CSS, além de serem respondidas algumas dúvidas sobre JavaScript.</p>
 </div>
 
-<div class="card">
-  <div class="imgBx">
-   <div>
-    <img src="../img/monitor.png">
-    <h2>Informática</h2>
-  </div>
-</div>
-<div class="overlay"></div>
-<div class="content">
-  <h2>Informática</h2>
-  <p>Matéria ministrada pelo docente Ranieri e apenas para os alunos do curso de Informática para Internet do 1º período, nela é ensinado o básico de HTML e CSS, além de serem respondidas algumas dúvidas sobre JavaScript.</p>
-</div>
-</div>
- <!-- <h1>Informações sobre docentes e cadeiras atualmente disponíveis no ProIFPE</h1>
-  <br>
-  <h2>ProIFPE Informática</h2>
-  <p>Matéria ministrada pelo docente Ranieri e apenas para os alunos do curso de Informática para Internet do 1º período, nela é ensinado o básico de HTML e CSS, além de serem respondidas algumas dúvidas sobre JavaScript</p>
-  <h2>ProIFPE Matemática</h2>
-  <p>Matéria ministrada pelo docente Cicero para alunos de Logística do 1º Período, e pelo professor Maurício, que ensina os alunos dos cursos de Logística e Informática para Internet do 1º período à tarde, na matéria são reforçados assuntos dados nas aulas normais</p>-->
- <!-- <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <a href="cadastro_professores.php" style="text-align: center;">Adicionar Professores</a>
-  <a href="cadastro_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a> -->
-
-</body>
-</html>
+<?php include 'footer.php' ?>
