304f263a8b561b4eddd72c492f2f1b5db1877d2b
Gabriella Batista <gabriella10batista@gmail.com>
2019-10-15 22:22:59 -0300

    adicionando classe ao formulário

--- projetos/proIFPE/login_student.php
+++ projetos/proIFPE/login_student.php
@@ -2,7 +2,7 @@
 
 <?php include "header.php" ?>
 
-<form method="post" action="login-process-student.php">
+<form class="form_info" method="post" action="login-process-student.php">
 nome <input type="text" name="nome"><br>
 matricula <input type="text" name="matricula"><br>
 <input type="submit" value="login">
--- projetos/proIFPE/login_teacher.php
+++ projetos/proIFPE/login_teacher.php
@@ -2,7 +2,7 @@
 
 <?php include "header.php" ?>
 
-<form method="post" action="login-process-teacher.php">
+<form class="form_info" method="post" action="login-process-teacher.php">
 nome <input type="text" name="nome"><br>
 email <input type="text" name="email"><br>
 <input type="submit" value="login">
