4208ebd35feb64af1667d23095e4126880429693
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-15 22:19:47 -0300

    corrigindo textos para ações

--- projetos/proIFPE/cadastro.php
+++ projetos/proIFPE/cadastro.php
@@ -7,5 +7,7 @@ include "config.php";
 
 
 <h2><a href="cad_professores.php" style="text-align: center;">Adicionar Professores</a></h2>
-<h2><a href="cad_disciplinas.php" style="text-align: center;">Adicionar Disciplinas</a></h2>
+<h2><a href="cad_disciplinas.php" style="text-align: center;">Adicionar Aulas</a></h2>
 <h2><a href="cad_alunos.php" style="text-align: center;">Adicionar Alunos</a></h2>
+
+<?php include 'footer.php' ?>
