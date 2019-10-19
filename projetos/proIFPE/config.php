fda1fcafc976b4a814340db33a9eb8bb7fa52fbd
Kaue Lui <ofckaue@gmail.com>
2019-10-19 17:04:07 -0300

    adicionando o arquivo csv de turmas

--- projetos/proIFPE/config.php
+++ projetos/proIFPE/config.php
@@ -3,6 +3,8 @@ define('PRO_FILE', 'src/professores.csv');
 define('DIS_FILE', 'src/disciplinas.csv');
 define('ALN_FILE', 'src/alunos.csv');
 define('NOT_FILE', 'src/notificacao.csv');
+define('TUR_FILE', 'src/turmas.csv');
+
 
 function redicect($url){
 	header('Location: ' . $url);
