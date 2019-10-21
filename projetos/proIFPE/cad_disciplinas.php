025ed42f823d6ba6e9b90eaf229342c7f8193e51
Eduarda19 <mariaeduardaj585@gmail.com>
2019-10-20 22:35:20 -0300

    comentando inputs antigos e adicionando legendas

--- projetos/proIFPE/cad_disciplinas.php
+++ projetos/proIFPE/cad_disciplinas.php
@@ -23,36 +23,43 @@ for ($i=0; $i < sizeof($dados); $i++) {
 <legend class="leg_form">Cadastro de Aula</legend>
 <form class="form_info" action="add_dis.php" method="POST" style="text-align: center;">
   <fieldset>
+    <legend>Dados da aula</legend>
     <!-- <input type="text" name="disciplina" required="" placeholder="Disciplina"><br>
     <input type="text" name="dia" placeholder="Dia"><br>
     <input type="text" name="horario" placeholder="Horário"><br> -->
-    <p>Disciplina:</p>
+    <p>Infome a disciplina:</p>
     <select name="disciplina">
-      <option value="" selected>Selecione a disciplina</option>
+      <option value="" selected disabled>Selecione a disciplina</option>
       <option value="Informática">Informática</option>
       <option value="Matemática">Matemática</option>
     </select>
-    <p>Dia:</p>
+    <p>Informe o dia:</p>
     <select name="dia">
-      <option value="" selected>Selecione o dia da semana</option>
+      <option value="" selected disabled>Selecione o dia da semana</option>
       <option value="Segunda">Segunda</option>
       <option value="Terça">Terça</option>
       <option value="Quarta">Quarta</option>
       <option value="Quinta">Quinta</option>
       <option value="Sexta">Sexta</option>
     </select>
-    <p>Turno:</p>
+    <p>informe o turno:</p>
     <select name="turno">
-      <option value="" selected>Selecione o turno</option>
+      <option value="" selected disabled>Selecione o turno</option>
       <option value="Manhã">Manhã</option>
       <option value="Tarde">Tarde</option>
     </select>
     <p>-----------------</p>
-    <p>Início da aula:</p><input type="time" name="inicio"><br>
-    <p>Término da aula:</p><input type="time" name="termino"><br>
+    <div>
+    <legend>Horário de início</legend>
+    <input type="time" name="inicio">
+    <br>
+    <br>
+    <legend>Horário de término</legend>
+    <input type="time" name="termino">
+    </div>
     <p>-----------------</p>
     <input type="submit" value="Adicionar">
-    <input type="reset" value="Limpar">
+    <!-- <input type="reset" value="Limpar"> -->
   </fieldset>
 </form>
 <br>
