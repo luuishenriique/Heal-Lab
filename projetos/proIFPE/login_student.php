8619badb9ddaffb8bfcfc67c3a2346d369d5da48
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-20 22:48:42 -0300

    adicionando fieldsets ao formulário e inserindo legendas para os inputs

--- projetos/proIFPE/login_student.php
+++ projetos/proIFPE/login_student.php
@@ -3,7 +3,13 @@
 <?php include "header.php" ?>
 
 <form class="form_info" method="post" action="login-process-student.php">
-nome <input type="text" name="nome"><br>
-matricula <input type="text" name="matricula"><br>
-<input type="submit" value="login">
+	<fieldset>
+		<legend>Informações para login</legend>
+		<!-- <input type="text" name="nome" placeholder="Ex: Utilize o login cadastrado"> -->
+		<legend>Matrícula do aluno</legend>
+		<input type="text" name="matricula" placeholder="Ex: 20191INFIG0001">
+		<legend>Senha do aluno</legend>
+		<input type="password" name="senha" placeholder="Ex: Log131704">
+		<input type="submit" value="Login">
+	</fieldset>
 </form>
