e2b56f08afa6304e7b38d2a534b83ac72185eb82
Kaue Lui <ofckaue@gmail.com>
2019-10-26 17:58:56 -0300

    adicionando pagina de login

---
+++ projetos/proIFPE/login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+	<link rel="shortcut icon" type="image/x-icon" href="img/5235fav.ico">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+</head>
+<body>
+
+
+	<div class="header">
+		<span id="nome">IFPE</span>
+		<h1 class="logo">Instituto Federal de Pernambuco</h1>
+		<span id="nome-1">ProIFPE - Campus Igarassu</span>
+	</div>
+	<div id="sobre">
+	</label>
+	<ul class="menu">
+		<a href="index.php">Início</a>
+		<a href="sobre.php">Sobre</a>
+	</ul>
+</label>
+
+</div>
+<form class="form_info" method="post" action="#">
+	<fieldset>
+		<legend>Login</legend>
+		<legend>Matrícula</legend>
+		<input type="text" name="matricula" placeholder="Ex: 20191INFIG0001">
+		<legend>Senha</legend>
+		<input type="password" name="senha" placeholder="Ex: Log131704">
+		<input type="submit" value="Login">
+	</fieldset>
+</form>
+
+</div>
+	
