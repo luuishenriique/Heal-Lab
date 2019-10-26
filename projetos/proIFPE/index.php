2f8d8028a16d5c8aa86170c3f684b9897bb53c59
Kaue Lui <ofckaue@gmail.com>
2019-10-26 17:59:59 -0300

    adicionando href para login

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -26,7 +26,7 @@
 	<h1>ProIFPE - Campus Igarassu</h1>
 	
 <div class="main">
-	<a id="main-a" href="#">
+	<a id="main-a" href="login.php">
 	LOGIN
 </a>
 	<a id="main-a" href="cadastro.php" style="border-top-width: 0px; border-top-style: 25px; margin-top: 10px;">
