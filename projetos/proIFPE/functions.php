d7e2792e880a35e60e8021ac05e1a491f55d750f
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-26 23:57:07 -0300

    inserindo arquivo de funções e movendo funções de config.php para este arquivo

---
+++ projetos/proIFPE/functions.php
@@ -0,0 +1,45 @@
+<?php 
+require 'config.php';
+
+function redirect($url){
+	header('Location: ' . $url);
+	exit();
+}
+
+function upperALL($str){
+	strtoupper($str);
+}
+
+function dbConnect(){
+	try {
+		$dsn = 'mysql:dbname=' . DB_NAME . ';host=' . DB_HOST . ';port=' . DB_PORT . ';charset=utf8';
+
+		$PDO = new PDO($dsn, DB_USER, DB_PASS);
+
+		return $PDO;
+	} catch (PDOException $e) {
+		echo 'Erro ao conectar com o MySql: ' . $e->getMessage();
+	}
+}
+
+function cryptPass($str){
+	return crypt($str,'lg');
+}
+
+function isLoggedIn(){
+	if (!isset($_SESSION['logado']) || $_SESSION['logado'] !== true) {
+		redirect('login.php');
+		exit();
+	}
+}
+
+function loggedOK(){
+	$_SESSION['logado'] = true;
+}
+
+function logOut(){
+	$_SESSION['logado'] = false;
+	session_destroy();
+	redirect('login.php');
+}
+?>
