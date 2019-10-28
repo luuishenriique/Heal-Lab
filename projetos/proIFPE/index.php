00799cad1912fce56d35538e360805e726bdc077
gabriellaBatista <gabriella10batista@gmail.com>
2019-10-27 22:38:09 -0300

    removendo link de cadastro de index

--- projetos/proIFPE/index.php
+++ projetos/proIFPE/index.php
@@ -1,39 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/estilo.css">
-	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="shortcut icon" type="image/x-icon" href="img/5235fav.ico">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-</head>
-<body>
-<style type="text/css">
-	
-.main-a{
- color:white;
-
-}
-</style>
-
-	<div class="header">
-		<span id="nome">IFPE</span>
-		<h1 class="logo">Instituto Federal de Pernambuco</h1>
-		<span id="nome-1">ProIFPE - Campus Igarassu</span>
-	</div>
+<?php include 'header.php'; ?>
 
 <title>proIFPE::Igarassu</title>
-	<h1>ProIFPE - Campus Igarassu</h1>
-	
+<h1>ProIFPE - Campus Igarassu</h1>
+
 <div class="main">
 	<a id="main-a" href="login.php">
-	LOGIN
-</a>
-	<a id="main-a" href="cadastro.php" style="border-top-width: 0px; border-top-style: 25px; margin-top: 10px;">
-		CADASTRO
+		LOGIN
 	</a>
-
-
+<!-- 	<a id="main-a" href="cadastro.php" style="border-top-width: 0px; border-top-style: 25px; margin-top: 10px;">
+		CADASTRO
+	</a> -->
 </div>
-	<?php include 'footer.php' ?>
+<?php include 'footer.php' ?>
