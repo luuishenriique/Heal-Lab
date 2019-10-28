016786011b265312b11e7d7976c57170b905e4eb
gabriellaBatista <gabriella10batista@gmail.com>
2019-10-27 22:39:10 -0300

    adicionando condições de header para tipos de usuário

--- projetos/proIFPE/header.php
+++ projetos/proIFPE/header.php
@@ -1,3 +1,7 @@
+<?php 
+require 'config.php';
+$tipo = $_SESSION['type_id'] ?? 1;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +13,6 @@
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
 </head>
 <body>
-
-
 	<div class="header">
 		<span id="nome">IFPE</span>
 		<h1 class="logo">Instituto Federal de Pernambuco</h1>
@@ -19,16 +21,48 @@
 	<div id="sobre">
 	</label>
 	<ul class="menu">
-		<a href="index.php">Início</a>
-		<a href="disciplinas.php">Professores</a>
-		<a href="horario.php">Horário</a>
-		<a href="cadastro.php">Cadastro</a>
-		<a href="sobre.php">Sobre</a>
-		<a href="notificacao.php">Notificação</a>
-		<!-- <a href="cad_turmas.php">Turmas</a> -->
+		<?php if ($_SESSION['type_id'] == 1): ?>
+			<strong><?= 'ADMINISTRADOR' ?></strong>
+			<a href="logout.php">Sair</a>
+			<a href="disciplinas.php">Apresentação</a>
+			<a href="horario.php">Horário</a>
+			<a href="cadastro.php">Cadastro</a>
+			<a href="cad_turmas.php">Turmas</a>
+			<a href="notificacao.php">Notificação</a>
+			<a href="sobre.php">Sobre</a>
+		<?php endif ?>
+		<?php if ($_SESSION['type_id'] == 2): ?>
+			<strong><?= 'PROFESSOR' ?></strong>
+			<a href="logout.php">Sair</a>
+			<a href="disciplinas.php">Apresentação</a>
+			<a href="horario.php">Horários</a>
+			<a href="cadastro.php" hidden>Cadastro</a>
+			<a href="cad_turmas.php">Turmas</a>
+			<a href="notificacao.php">Notificação</a>
+			<a href="sobre.php">Sobre</a>
+		<?php endif ?>
+		<?php if ($_SESSION['type_id'] == 3): ?>
+			<strong><?= 'ALUNO' ?></strong>
+			<a href="logout.php">Sair</a>
+			<a href="disciplinas.php">Apresentação</a>
+			<a href="horario.php">Horários</a>
+			<a href="cadastro.php" hidden>Cadastro</a>
+			<a href="cad_turmas.php" hidden>Turmas</a>
+			<a href="notificacao.php">Notificação</a>
+			<a href="sobre.php">Sobre</a>
+		<?php endif ?>
+		<?php if (is_null($_SESSION['type_id'])): ?>
+			<strong>| <?= 'VISITANTE' ?> |</strong>
+			<a href="index.php">Início</a> | 
+			<a href="disciplinas.php">Apresentação</a> |
+			<a href="horario.php">Horários</a> |
+			<a href="cadastro.php" hidden>Cadastro</a>
+			<a href="cad_turmas.php" hidden>Turmas</a>
+			<a href="notificacao.php" hidden>Notificação</a>
+			<a href="sobre.php">Sobre</a> |	
+		<?php endif ?>	
 	</ul>
 </label>
-
 </div>
 
 
