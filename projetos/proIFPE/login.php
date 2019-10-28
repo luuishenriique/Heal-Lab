f94f32d5a45d5635600291fa5893e240be15f56e
MARIA617 <mariac4682@gmail.com>
2019-10-27 22:44:39 -0300

    adicionando inputs e ajustando formulário de login

--- projetos/proIFPE/login.php
+++ projetos/proIFPE/login.php
@@ -1,42 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/estilo.css">
-	<link href="https://fonts.googleapis.com/css?family=Sintony&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
-	<link rel="shortcut icon" type="image/x-icon" href="img/5235fav.ico">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-</head>
-<body>
-
-	<title>Login</title>
-	<div class="header">
-		<span id="nome">IFPE</span>
-		<h1 class="logo">Instituto Federal de Pernambuco</h1>
-		<span id="nome-1">ProIFPE - Campus Igarassu</span>
-	</div>
-	<div id="sobre">
-	</label>
-	<ul class="menu">
-		<a href="index.php">Início</a>
-		<a href="sobre.php">Sobre</a>
-	</ul>
-</label>
-
-</div>
+<?php include 'header.php'; ?>
+	<title>proIFPE::Login</title>
 <br>
-<form class="form_info" method="POST" action="#">
+<form class="form_info" method="POST" action="auth_user.php">
 	<fieldset>
 		<legend>Área de Login</legend>
-		<legend>Matrícula</legend>
-		<input type="text" name="matricula" placeholder="Ex: 20191INFIG0001" required>
+		<legend>Matrícula ou SIAPE</legend>
+		<input type="text" name="matricula" required>
 		<legend>Senha</legend>
-		<input type="password" name="senha" placeholder="Ex: Log131704" required>
+		<input type="password" name="senha" required>
 		<input type="submit" value="Login">
 		<p>Primeiro login? <br> <a href="att_nuser.php">Habilite seus dados aqui!</a></p>
 	</fieldset>
 </form>
-
 </div>
+<br>
+<h3><a href="index.php">Voltar</a></h3>
+<?php include 'footer.php'; ?>
 
