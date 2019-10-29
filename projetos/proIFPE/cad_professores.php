7dd1f9939aec5f3e93eb0fe3ea05060c3f59ce4b
Eduarda19 <mariaeduardaj585@gmail.com>
2019-10-28 22:59:35 -0300

    habilitando visualização de dados via banco com PDO

--- projetos/proIFPE/cad_professores.php
+++ projetos/proIFPE/cad_professores.php
@@ -7,73 +7,66 @@ include 'config.php';
 
 <?php
 
-$dado = file(DIS_FILE);
+// $dado = file(DIS_FILE);
 
-$dados = file(PRO_FILE);
+// $dados = file(PRO_FILE);
 
-$professor = [];
+// $professor = [];
 
-for ($i=0; $i < sizeof($dados); $i++) { 
-	$dados[$i] = explode(',', $dados[$i]);
+// for ($i=0; $i < sizeof($dados); $i++) { 
+// 	$dados[$i] = explode(',', $dados[$i]);
+// }
+
+session_start();
+
+$PDO = dbConnect();
+
+$sql = "SELECT * FROM Professores";
+
+$stmt = $PDO->prepare($sql);
+
+$stmt->execute();
+
+$linhas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($linhas);
+
+if (count($linhas) <= 0) {
+  // redirect('cad_disciplinas.php');
+	echo 'Info::Achei nada!';
+	exit();
 }
-
 ?>
-
-<?php if (!empty($_GET['msg'])): ?>
-	<div class="msg_info">
-		<h3><?= $_GET['msg'] ?></h3>
-	</div>
-<?php endif ?>
-
-<br>
-<form class="form_info" action="add_prof.php" method="POST" style="text-align: center;">
-	<fieldset>
-		<legend class="leg_form">Informações sobre o professor</legend>
-		<legend>SIAPE do professor</legend>
-		<input type="text" name="siape" placeholder="Ex: ABC123CB">
-		<legend>Nome do professor</legend>
-		<input type="text" name="professor" placeholder="Ex: Marcos Paulo"><br>
-		<legend>Email do professor</legend>
-		<input type="text" name="email" placeholder="Ex: paulo.marcos@gmail.com"><br>
-
-     	<div>
-    	<legend>Disciplina</legend>
-    	<select type="text" name="select-disciplina">
-        <?php foreach($dado as $dd): ?>
-            <option value="<? $dd ?>"><?= trim($dd) ?></option>
-        <?php endforeach ?>
-      	</select>
-    	</div>
-
-		<p>--------------</p>
-		<input type="submit" value="Adicionar">
-		<!-- <input type="reset" value="Limpar"> -->
-	</fieldset>
-</form>
 <br>
 <h2>Professores cadastrados</h2>
 <br>
 <table>
-	<tr>
-		<th>SIAPE</th>
-		<th>Professor</th>
-		<th>Disciplina</th>
-		<th>Email</th>
-		<th>Apagar Professor</th>
-		<th>Editar</th>
-	</tr>
-	<?php foreach ($dados as $i => $dado): ?>
+	<thead>
 		<tr>
-			<?php foreach ($dado as $dados): ?>
-				<td><?= $dados ?></td>
-			<?php endforeach  ?>
-    <td>
-      <a href="del_prof.php?linha=<?= $i ?>" class="btn"><i class="far fa-trash-alt"></i>
-      </td>
-      <td>
-        <a href="edi_prof.php?linha=<?= $id ?>" class="btn"><i class="far fa-edit"></i></a>
-      </td>
-    </tr>
-		<?php endforeach ?>
-	</table>
-	<?php include 'footer.php' ?>
+			<th>SIAPE</th>
+			<th>Nome do professor</th>
+			<th>Email de contato</th>
+			<th>Nível de acesso</th>
+			<th>Ações</th>
+		</tr>
+	</thead>
+	<tbody>
+		
+	</tbody>
+	<?php foreach ($linhas as $id => $linha): ?>
+		<tr>
+			<td><?= $linhas[$id]['siape_prof'] ?></td>
+			<td><?= $linhas[$id]['name_prof'] ?></td>
+			<td><?= $linhas[$id]['email_prof'] ?></td>
+			<td><?= $linhas[$id]['id_user'] ?></td>
+			<td>
+				<nav>
+					<a href="">&#133;</a> |
+					<a href="">&times;</a>
+				</nav>
+			</td>
+		</tr>
+	<?php endforeach ?>
+</table>
+<br>
+<h4 style="text-align: center;"><a href="home.php">Voltar para home</a></h4>
+<?php include 'footer.php' ?>
