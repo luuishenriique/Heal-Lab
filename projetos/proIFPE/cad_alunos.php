c0ea02693006059b9dc0f7406a3006cc360e31be
Eduarda19 <mariaeduardaj585@gmail.com>
2019-10-28 22:57:39 -0300

    modificando página para listagem de alunos via banco de dados

--- projetos/proIFPE/cad_alunos.php
+++ projetos/proIFPE/cad_alunos.php
@@ -3,14 +3,23 @@
 <?php 
 include 'header.php';
 include 'config.php';
+session_start();
 
-$dados_turma = file(TUR_FILE);
-$dados_cadeira = file(CDR_FILE);
-$dados_curso = file(CUR_FILE);
+$PDO = dbConnect();
 
-$dados = file(ALN_FILE);
-for ($i = 0; $i < sizeof($dados); $i++) {
-	$dados[$i] = explode(',', $dados[$i]);
+$sql = "SELECT * FROM Alunos";
+
+$stmt = $PDO->prepare($sql);
+
+$stmt->execute();
+
+$linhas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($linhas);
+
+if (count($linhas) <= 0) {
+	redirect('login.php');
+	echo 'Achei nada!';
+	exit();
 }
 ?>
 
@@ -21,69 +30,38 @@ for ($i = 0; $i < sizeof($dados); $i++) {
 <?php endif ?>
 
 <br>
-<legend class="leg_form">Informações sobre o aluno</legend>
-<form class="form_info" action="add_aln.php" method="POST">
-	<fieldset>
-		<legend>Informações sobre o aluno</legend>
-		<legend>Matrícula do aluno</legend>
-		<input type="text" name="matricula" placeholder="Ex: 20172LOG3322" required>
-		<legend>Nome do aluno</legend>
-		<input type="text" name="aluno" placeholder="Ex: João da Silva" required>
-		<legend>Email do aluno</legend>
-		<input type="text" name="email" placeholder="Ex: joao.silva@outlook.com" required>
-		<label>Informe o curso:</label>
-		<select name="curso">
-			<option value="" selected disabled>Selecione o curso</option>
-			<?php foreach ($dados_curso as $cur): ?>
-				<option value="<?= $cur ?>"><?= $cur ?></option>
-			<?php endforeach ?>
-		</select>
-		<br>
-		<br>
-		<label>Informe a disciplina:</label>
-		<select name="disciplina">
-			<option value="" selected disabled>Selecione a disciplina</option>
-			<?php foreach ($dados_cadeira as $cdr): ?>
-				<option value="<?= $cdr ?>"><?= $cdr ?></option>
-			<?php endforeach ?>
-		</select>
-		<br>
-		<br>
-		<label>Informe a turma:</label>
-		<select name="turma">
-			<option value="" selected disabled>Selecione a turma</option>
-			<?php foreach ($dados_turma as $tur): ?>
-				<option value="<?= $tur ?>"><?= $tur ?></option>
-			<?php endforeach ?>
-		</select>
-		<p>---------------------</p>
-		<!-- <input type="date" name="data" placeholder="Data de entrada"> -->
-		<input type="submit" value="Adicionar">
-		<!-- <input type="reset" value="Limpar"> -->
-	</fieldset>
-</form>
-<br>
 <h2>Alunos cadastrados</h2>
 <br>
 <table>
-	<tr>
-		<th>Matrícula</th>
-		<th>Nome do Aluno</th>
-		<th>Email</th>
-		<th>Curso</th>
-		<th>Disciplina</th>
-		<th>Apagar Aluno</th>
-	</tr>
-	<?php foreach ($dados as $i => $dado): ?>
+	<thead>
 		<tr>
-			<?php foreach ($dado as $dados): ?>
-				<td><?= $dados ?></td>
-			<?php endforeach ?>
-			<td>
-				<a class="btn" href="del_aln.php?linha=<?= $i ?>"><i class="far fa-trash-alt"></i></a>
-			</td>
-		</tr>
-	<?php endforeach ?>
+			<th>Matrícula</th>
+			<th>Nome do Aluno</th>
+			<th>Email de contato</th>
+			<th>Curso</th>
+			<th>Turma</th>
+			<th>Ãções</th>
+		</tr>	
+	</thead>
+	<tbody>
+		<?php foreach ($linhas as $id => $linha): ?>
+			<tr>
+				<td><?= $linhas[$id]['mat_aluno'] ?></td>
+				<td><?= $linhas[$id]['name_aluno'] ?></td>
+				<td><?= $linhas[$id]['email_aluno'] ?></td>
+				<td></td>
+				<td></td>
+				<td>
+					<nav>
+						<a href="">&#133;</a> |
+						<a href="">&times;</a>
+					</nav>
+				</td>
+			</tr>
+		<?php endforeach ?>
+	</tbody>
 </table>
+<br>
+<h4 style="text-align: center;"><a href="home.php">Voltar para home</a></h4>
 <?php include 'footer.php' ?>
 
