8f6f1c785d993a43aa9a77388042b7eeaeecc785
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-28 22:48:48 -0300

    adicionando arquivo de listagem de administradores

---
+++ projetos/proIFPE/cad_administradores.php
@@ -0,0 +1,63 @@
+<title>proIFPE::Cadastro de Administradores</title>
+
+<?php 
+include 'header.php';
+include 'config.php';
+?>
+
+<?php
+session_start();
+
+$PDO = dbConnect();
+
+$sql = "SELECT * FROM Administradores";
+
+$stmt = $PDO->prepare($sql);
+
+$stmt->execute();
+
+$linhas = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// var_dump($linhas);
+
+if (count($linhas) <= 0) {
+  // redirect('cad_administradores.php');
+	echo 'Info::Achei nada!';
+	exit();
+}
+?>
+<br>
+<h2>Administradores cadastrados</h2>
+<br>
+<table>
+	<thead>
+		<tr>
+			<th>Código</th>
+			<th>Nome do administrador</th>
+			<th>Login</th>
+			<th>Email de contato</th>
+			<th>Nível de acesso</th>
+			<th>Ações</th>
+		</tr>
+	</thead>
+	<tbody>
+		
+	</tbody>
+	<?php foreach ($linhas as $id => $linha): ?>
+		<tr>
+			<td><?= $linhas[$id]['id_adm'] ?></td>
+			<td><?= $linhas[$id]['name_adm'] ?></td>
+			<td><?= $linhas[$id]['login_adm'] ?></td>
+			<td><?= $linhas[$id]['email_adm'] ?></td>
+			<td><?= $linhas[$id]['id_user'] ?></td>
+			<td>
+				<nav>
+					<a href="">&#133;</a> |
+					<a href="">&times;</a>
+				</nav>
+			</td>
+		</tr>
+	<?php endforeach ?>
+</table>
+<br>
+<h4 style="text-align: center;"><a href="home.php">Voltar para home</a></h4>
+<?php include 'footer.php' ?>
