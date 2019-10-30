f0041e079e77f2b023f0a723675bece05559727b
Luis Henrique <luis_henrique_co@hotmail.com>
2019-10-29 22:34:35 -0300

    ajustando referência em links de notificação

--- projetos/proIFPE/home.php
+++ projetos/proIFPE/home.php
@@ -1,9 +1,9 @@
 <?php 
-include 'header.php';
 session_start();
+require 'config.php';
+include 'header.php';
 isLoggedIn();
 $tipo = $_SESSION['type_id'];
-require 'config.php';
 ?>
 <title>proIFPE::Home</title>
 <h3>Bem vindo <?= $_SESSION['user_name']?>!</h3>
@@ -20,6 +20,7 @@ require 'config.php';
 <?php if ($_SESSION['type_id'] == 2): ?>
 	<h2><a href="">Suas turmas</a></h2>
 	<h2><a href="">Suas aulas</a></h2>
+	<h2><a href="notificacao.php">Ver notificações</a></h2>
 	<h2><a href="my_data.php">Perfil</a></h2>
 	<h3><a href="logout.php">Sair</a></h3>
 <?php endif ?>
@@ -27,6 +28,7 @@ require 'config.php';
 		<h2><a href="">Sua turma</a></h2>
 		<h2><a href="">Suas aulas</a></h2>
 		<h2><a href="">Boletim</a></h2>
+		<h2><a href="notificacao.php">Ver notificações</a></h2>
 		<h2><a href="my_data.php">Perfil</a></h2>
 		<h3><a href="logout.php">Sair</a></h3>
 <?php endif ?>
