e27d3cedfd32a981ce77a069a53002d5f60da6bc
gabriellaBatista <gabriella10batista@gmail.com>
2019-11-11 22:46:18 -0300

    removendo botão inferior de sair de home.php

--- projetos/proIFPE/home.php
+++ projetos/proIFPE/home.php
@@ -6,7 +6,7 @@ isLoggedIn();
 $tipo = $_SESSION['type_id'];
 ?>
 <title>proIFPE::Home</title>
-<h3>Bem vindo <?= $_SESSION['user_name']?>!</h3>
+<h3>Bem vindo, <?= $_SESSION['user_name']?>!</h3>
 <br>
 <?php if ($_SESSION['type_id'] == 1): ?>
 	<h2><a href="">Cadastrar turmas</a></h2>
@@ -15,14 +15,14 @@ $tipo = $_SESSION['type_id'];
 	<h2><a href="cad_disciplinas.php">Cadastrar disciplinas</a></h2>
 	<h2><a href="cad_administradores.php">Cadastrar administradores</a></h2>
 	<h2><a href="my_data.php">Perfil</a></h2>
-	<h3><a href="logout.php">Sair</a></h3>
+<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
 <?php if ($_SESSION['type_id'] == 2): ?>
 	<h2><a href="">Suas turmas</a></h2>
 	<h2><a href="">Suas aulas</a></h2>
 	<h2><a href="notificacao.php">Ver notificações</a></h2>
 	<h2><a href="my_data.php">Perfil</a></h2>
-	<h3><a href="logout.php">Sair</a></h3>
+<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
 <?php if ($_SESSION['type_id'] == 3): ?>
 		<h2><a href="">Sua turma</a></h2>
@@ -30,7 +30,7 @@ $tipo = $_SESSION['type_id'];
 		<h2><a href="">Boletim</a></h2>
 		<h2><a href="notificacao.php">Ver notificações</a></h2>
 		<h2><a href="my_data.php">Perfil</a></h2>
-		<h3><a href="logout.php">Sair</a></h3>
+	<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
 <br>
 <?php include 'footer.php'; ?>
