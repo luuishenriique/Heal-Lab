a4fcc58d90371799a0b8ebea666945054bd47bc8
Eduarda19 <mariaeduardaj585@gmail.com>
2019-11-12 23:46:49 -0300

    removendo opções ainda indisponíveis do home

--- projetos/proIFPE/home.php
+++ projetos/proIFPE/home.php
@@ -9,7 +9,7 @@ $tipo = $_SESSION['type_id'];
 <h3>Bem vindo, <?= $_SESSION['user_name']?>!</h3>
 <br>
 <?php if ($_SESSION['type_id'] == 1): ?>
-	<h2><a href="">Cadastrar turmas</a></h2>
+	<h2><a href="cad_turmas.php">Cadastrar turmas</a></h2>
 	<h2><a href="cad_alunos.php">Cadastrar alunos</a></h2>
 	<h2><a href="cad_professores.php">Cadastrar professores</a></h2>
 	<h2><a href="cad_disciplinas.php">Cadastrar disciplinas</a></h2>
@@ -18,17 +18,17 @@ $tipo = $_SESSION['type_id'];
 <!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
 <?php if ($_SESSION['type_id'] == 2): ?>
-	<h2><a href="">Suas turmas</a></h2>
-	<h2><a href="">Suas aulas</a></h2>
-	<h2><a href="notificacao.php">Ver notificações</a></h2>
+	<!-- <h2><a href="">Suas turmas</a></h2> -->
+	<h2><a href="cad_aula.php">Suas aulas</a></h2>
+	<!-- <h2><a href="notificacao.php">Ver notificações</a></h2> -->
 	<h2><a href="my_data.php">Perfil</a></h2>
 <!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
 <?php if ($_SESSION['type_id'] == 3): ?>
-		<h2><a href="">Sua turma</a></h2>
-		<h2><a href="">Suas aulas</a></h2>
+		<!-- <h2><a href="">Sua turma</a></h2> -->
+		<h2><a href="aln_classes">Suas aulas</a></h2>
 		<h2><a href="">Boletim</a></h2>
-		<h2><a href="notificacao.php">Ver notificações</a></h2>
+		<!-- <h2><a href="notificacao.php">Ver notificações</a></h2> -->
 		<h2><a href="my_data.php">Perfil</a></h2>
 	<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
