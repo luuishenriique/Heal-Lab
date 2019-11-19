18710aacbc62454bf8e5a5496b387c405731be53
Luis Henrique <luis_henrique_co@hotmail.com>
2019-11-18 22:59:43 -0300

    atualizando base de dados do banco

--- projetos/proIFPE/db/proIFPEdb.sql
+++ projetos/proIFPE/db/proIFPEdb.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.28, for Linux (x86_64)
 --
 -- Host: localhost    Database: proIFPE
 -- ------------------------------------------------------
--- Server version	5.7.27-0ubuntu0.18.04.1
+-- Server version	5.7.28-0ubuntu0.18.04.4
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -59,11 +59,17 @@ CREATE TABLE `Alunos` (
   `email_aluno` varchar(50) DEFAULT NULL,
   `password_aluno` varchar(50) DEFAULT NULL,
   `id_user` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
+  `id_turma` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_aluno`),
   UNIQUE KEY `mat_aluno` (`mat_aluno`),
   KEY `FK_Usuarios_Alunos` (`id_user`),
+  KEY `FK_Alunos_Cursos` (`id_curso`),
+  KEY `FK_Alunos_Turmas` (`id_turma`),
+  CONSTRAINT `FK_Alunos_Cursos` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  CONSTRAINT `FK_Alunos_Turmas` FOREIGN KEY (`id_turma`) REFERENCES `Turmas` (`id_turma`),
   CONSTRAINT `FK_Usuarios_Alunos` FOREIGN KEY (`id_user`) REFERENCES `Usuarios` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -72,7 +78,7 @@ CREATE TABLE `Alunos` (
 
 LOCK TABLES `Alunos` WRITE;
 /*!40000 ALTER TABLE `Alunos` DISABLE KEYS */;
-INSERT INTO `Alunos` VALUES (1,'Luis Henrique Chaves de Oliveira','20191INFIG0201','luis_henrique_co@hotmail.com','lghFqNRvZ/mA2',3),(3,'Renata Barbosa de Lima','20181LG3334','rena.barbosa@gmail.com','lgqhVuytGZvrs',3),(4,'Gabriel Barros Teixeira','20191INFIG0309','gabrielbarrosteixeira9@gmail.com','lgm9odYL3FXpw',3);
+INSERT INTO `Alunos` VALUES (5,'LUIS HENRIQUE CHAVES DE OLIVEIRA','20191INFIG0201','luis_henrique_co@hotmail.com','lghFqNRvZ/mA2',3,1,NULL);
 /*!40000 ALTER TABLE `Alunos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -86,10 +92,13 @@ DROP TABLE IF EXISTS `Aulas`;
 CREATE TABLE `Aulas` (
   `id_aula` int(11) NOT NULL AUTO_INCREMENT,
   `data_aula` date DEFAULT NULL,
-  `cod_prof` int(11) NOT NULL,
   `desc_aula` varchar(4000) NOT NULL,
+  `id_prof` int(11) NOT NULL,
+  `id_turma` int(11) NOT NULL,
+  `id_disc` int(11) NOT NULL,
+  `id_curso` int(11) NOT NULL,
   PRIMARY KEY (`id_aula`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -98,7 +107,7 @@ CREATE TABLE `Aulas` (
 
 LOCK TABLES `Aulas` WRITE;
 /*!40000 ALTER TABLE `Aulas` DISABLE KEYS */;
-INSERT INTO `Aulas` VALUES (1,'2019-11-01',10000,'Aula 01 - Aprendendo HTML');
+INSERT INTO `Aulas` VALUES (1,'2019-11-01','Aula 01 - Aprendendo HTML',0,0,0,0),(2,'2017-07-31','dfsdf',0,0,0,0);
 /*!40000 ALTER TABLE `Aulas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -139,9 +148,14 @@ CREATE TABLE `Disciplinas` (
   `id_disc` int(11) NOT NULL AUTO_INCREMENT,
   `name_disc` varchar(150) NOT NULL,
   `desc_disc` varchar(4000) DEFAULT NULL,
+  `id_prof` int(11) DEFAULT NULL,
   `id_curso` int(11) NOT NULL,
-  PRIMARY KEY (`id_disc`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id_disc`),
+  KEY `FK_Professores_Disciplinas` (`id_prof`),
+  KEY `FK_Cursos_Disciplinas` (`id_curso`),
+  CONSTRAINT `FK_Cursos_Disciplinas` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  CONSTRAINT `FK_Professores_Disciplinas` FOREIGN KEY (`id_prof`) REFERENCES `Professores` (`id_prof`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -150,7 +164,7 @@ CREATE TABLE `Disciplinas` (
 
 LOCK TABLES `Disciplinas` WRITE;
 /*!40000 ALTER TABLE `Disciplinas` DISABLE KEYS */;
-INSERT INTO `Disciplinas` VALUES (1,'A','AAA',0),(2,'b','bbb',0);
+INSERT INTO `Disciplinas` VALUES (3,'Informática','Conceitos básicos de programação web e revisão de conteúdo de cadeiras de sala de aula.',NULL,1),(4,'Matemática','Base de cálculos básica, funções, vetores, plano cartesiano.',NULL,1),(5,'Matemática','Aprender a calcular e conceitos básicos de funções',NULL,2);
 /*!40000 ALTER TABLE `Disciplinas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -165,6 +179,7 @@ CREATE TABLE `Matriculas` (
   `id_aluno` int(11) NOT NULL,
   `id_turma` int(11) NOT NULL,
   `id_curso` int(11) NOT NULL,
+  `data_matricula` date NOT NULL,
   PRIMARY KEY (`id_aluno`,`id_turma`),
   KEY `FK_Matriculas_Turmas` (`id_turma`),
   KEY `FK_Matriculas_Cursos` (`id_curso`),
@@ -197,10 +212,13 @@ CREATE TABLE `Professores` (
   `email_prof` varchar(50) DEFAULT NULL,
   `password_prof` varchar(50) DEFAULT NULL,
   `id_user` int(11) NOT NULL,
+  `id_disc` int(11) NOT NULL,
   PRIMARY KEY (`id_prof`),
   KEY `FK_Usuarios_Professores` (`id_user`),
+  KEY `FK_Disciplinas_Professores` (`id_disc`),
+  CONSTRAINT `FK_Disciplinas_Professores` FOREIGN KEY (`id_disc`) REFERENCES `Disciplinas` (`id_disc`),
   CONSTRAINT `FK_Usuarios_Professores` FOREIGN KEY (`id_user`) REFERENCES `Usuarios` (`id_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=10001 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -209,7 +227,6 @@ CREATE TABLE `Professores` (
 
 LOCK TABLES `Professores` WRITE;
 /*!40000 ALTER TABLE `Professores` DISABLE KEYS */;
-INSERT INTO `Professores` VALUES (10000,'1234567','João Kleber Para Para','parapara@gmail.com','lglG.QxZgvFNA',2);
 /*!40000 ALTER TABLE `Professores` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -226,10 +243,16 @@ CREATE TABLE `Turmas` (
   `id_curso` int(11) NOT NULL,
   `nome_turma` varchar(15) NOT NULL,
   `capacidade_turma` int(3) DEFAULT NULL,
+  `id_prof` int(11) DEFAULT NULL,
+  `id_disc` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_turma`),
   KEY `FK_Curso_Turma` (`id_curso`),
-  CONSTRAINT `FK_Curso_Turma` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+  KEY `FK_Professores_Turmas` (`id_prof`),
+  KEY `FK_Disciplinas_Turmas` (`id_disc`),
+  CONSTRAINT `FK_Curso_Turma` FOREIGN KEY (`id_curso`) REFERENCES `Cursos` (`id_curso`),
+  CONSTRAINT `FK_Disciplinas_Turmas` FOREIGN KEY (`id_disc`) REFERENCES `Disciplinas` (`id_disc`),
+  CONSTRAINT `FK_Professores_Turmas` FOREIGN KEY (`id_prof`) REFERENCES `Professores` (`id_prof`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -238,7 +261,6 @@ CREATE TABLE `Turmas` (
 
 LOCK TABLES `Turmas` WRITE;
 /*!40000 ALTER TABLE `Turmas` DISABLE KEYS */;
-INSERT INTO `Turmas` VALUES (1,'Manhã',1,'20191INFIG01',32);
 /*!40000 ALTER TABLE `Turmas` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -275,4 +297,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-13  0:10:41
+-- Dump completed on 2019-11-18 22:57:49
