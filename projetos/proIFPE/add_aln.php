b8ebb8f4e7be1e8f050513f1c5356b89b16c4466
Luis Henrique <luis_henrique_co@hotmail.com>
2019-11-18 23:01:38 -0300

    adicionando variáveis de checagem de matrículas e ajustes na inserção de dados no banco

--- projetos/proIFPE/add_aln.php
+++ projetos/proIFPE/add_aln.php
@@ -22,30 +22,54 @@ session_start();
 // }
 
 $matricula = $_POST['matricula'] ?? false;
+$matricula = strtoupper($matricula); /*convertendo para maiúsculo*/
 $nome = $_POST['nome_aln'] ?? false;
-$curso = $_POST['select-curso'] ?? false;
+$nome = strtoupper($nome); /*convertendo para maiúsculo*/
+// $curso = $_POST['select-curso'] ?? false;
+$iduser = 3; /*usuário tipo aluno*/
+$findINF = "INF";
+$findLOG = "LOG";
+$checkINF = checkString($findINF, $matricula);
+$checkLOG = checkString($findLOG, $matricula);
 $idcurso = 0;
-$iduser = 3;
+// $data = date("d.m.Y");                      
 
-if ($curso == "INFORMÁTICA PARA INTERNET") {
+// if ($curso == "INFORMÁTICA PARA INTERNET") {
+// 	$idcurso = 1;
+// }
+
+// if ($curso == "LOGÍSTICA") {
+// 	$idcurso = 2;
+// }
+
+if ($checkINF === true) {
 	$idcurso = 1;
+	// echo 'Informática';
+	// exit();
 }
 
-if ($curso == "LOGÍSTICA") {
+if ($checkLOG === true) {
 	$idcurso = 2;
+	// echo 'Logística';
+	// exit();
+}
+
+if (checkINF === false && checkLOG === false) {
+	redirect('form_aln.php?msg=Matrícula não aceita');
+	exit();
 }
 
 $PDO = dbConnect();
 
-$sql = "INSERT INTO Alunos(mat_aluno,name_aluno,id_user) values(:matricula,:nome,:iduser)";
+$sql = "INSERT INTO Alunos(mat_aluno,name_aluno,id_user,id_curso) VALUES(:matricula,:nome,:iduser,:idcurso)";
 
 
 $stmt = $PDO->prepare($sql);
 
-$matricula = strtoupper($matricula);
 $stmt->bindParam(':matricula', $matricula);
 $stmt->bindParam(':nome', $nome);
 $stmt->bindParam(':iduser', $iduser);
+$stmt->bindParam(':idcurso', $idcurso);
 
 $stmt->execute();
 
