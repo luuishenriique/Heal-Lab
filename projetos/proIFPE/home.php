931535fd469d8cd77716dd270f322a5368b586c7
KaueLui <ofckaue@gmail.com>
2019-11-24 23:43:06 -0300

    adicionando referências para opções em home.php

--- projetos/proIFPE/home.php
+++ projetos/proIFPE/home.php
@@ -19,7 +19,7 @@ $tipo = $_SESSION['type_id'];
 <?php endif ?>
 <?php if ($_SESSION['type_id'] == 2): ?>
 	<!-- <h2><a href="">Suas turmas</a></h2> -->
-	<h2><a href="cad_aula.php">Suas aulas</a></h2>
+	<h2><a href="my_classes_prof.php">Suas aulas</a></h2>
 	<!-- <h2><a href="notificacao.php">Ver notificações</a></h2> -->
 	<h2><a href="my_data.php">Perfil</a></h2>
 <!-- 	<h3><a href="logout.php">Sair</a></h3> -->
