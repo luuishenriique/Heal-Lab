6c8bf876d232612a322f88950e8e44c3a7619100
MARIA617 <mariac4682@gmail.com>
2019-11-24 23:30:22 -0300

    inserindo variáveis na sessão e no get

--- projetos/proIFPE/cad_aula.php
+++ projetos/proIFPE/cad_aula.php
@@ -5,13 +5,17 @@ session_start();
 include 'header.php';
 include 'config.php';
 
+$idprof = $_SESSION['user_id'];
+$idturma = $_GET['id'];
+
 $PDO = dbConnect();
 
-$sql = "SELECT * FROM Aulas WHERE cod_prof = :id";
+$sql = "SELECT * FROM Aulas WHERE id_prof = :idprof AND nome_turma = :idturma";
 
 $stmt = $PDO->prepare($sql);
 
-$stmt->bindParam(':id', $_SESSION['user_id']);
+$stmt->bindParam(':idprof', $idprof);
+$stmt->bindParam(':idturma', $idturma);
 
 $stmt->execute();
 
@@ -33,15 +37,15 @@ if (count($linhas) <= 0) {
 <?php endif ?>
 
 <br>
-<h2>Aulas cadastradas</h2>
+<h2>Aulas cadastradas para turma</h2>
 <br>
 <table>
 	<thead>
 		<tr>
-			<td>Aula</td>
-			<td>Data</td>
-			<td>Descrição</td>
-			<!-- <td></td> -->
+			<th>Aula</th>
+			<th>Data</th>
+			<th>Descrição</th>
+			<th>Ações</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -55,6 +59,7 @@ if (count($linhas) <= 0) {
 	</tbody>
 </table>
 <br>
-<h3 style="text-align: center;"><a href="form_aula.php">Adicionar nova aula</a></h3>
+<h3 style="text-align: center;"><a href="form_aula.php?id=<?= $idturma ?>">Adicionar nova aula</a></h3>
+<h3 style="text-align: center;"><a href="alunos_aula.php?id=<?= $idturma ?>">Ver alunos</a></h3>
 <h4 style="text-align: center;"><a href="home.php">Voltar para home</a></h4>
 <?php include 'footer.php' ?>
