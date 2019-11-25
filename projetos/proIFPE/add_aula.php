d0e721c8c08983e371e5c73701c3002b842cef44
Mateuslpds <mateusyasmim2014@gmail.com>
2019-11-24 23:05:45 -0300

    modificando query sql

--- projetos/proIFPE/add_aula.php
+++ projetos/proIFPE/add_aula.php
@@ -2,20 +2,30 @@
 include 'config.php';
 session_start();
 
-$data = $_POST['data'] ?? false;
+// $data = date();
+// echo $date;
+// exit();
+
 $descricao = $_POST['descricao'] ?? false;
 
 $PDO = dbConnect();
 
-$sql = "INSERT INTO Aulas(data_aula,desc_aula,cod_prof) values(:data,:descricao,:cod_prof)";
+$sql = "INSERT INTO Aulas(data_aula,desc_aula,id_prof,id_turma) values(:data,:descricao,:idprof,:idturma)";
 
 $stmt = $PDO->prepare($sql);
 
 $stmt->bindParam(':data', $data);
 $stmt->bindParam(':descricao', $descricao);
-$stmt->bindParam(':cod_prof', $_SESSION['user_id']);
+$stmt->bindParam(':idprof', $_SESSION['user_id']);
+$stmt->bindParam(':idturma', $_SESSION['idturma']);
+// $stmt->bindParam(':iddisc', $_SESSION['iddisc']);
+// $stmt->bindParam(':idcurso', $_SESSION['idcurso']);
+
 
 $stmt->execute();
-
+// echo $_SESSION['user_id'];
+// echo $_SESSION['idturma'];
+// echo $_SESSION['iddisc'];
+// echo $_SESSION['user_id'];
 redirect('cad_aula.php');
 ?>
