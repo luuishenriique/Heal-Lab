24ea62279133eb7accc9f68976b94b1527236d35
MARIA617 <mariac4682@gmail.com>
2019-12-04 23:12:32 -0300

    dando efeitos de cards as opções e separando por seções

--- projetos/proIFPE/home.php
+++ projetos/proIFPE/home.php
@@ -6,31 +6,103 @@ isLoggedIn();
 $tipo = $_SESSION['type_id'];
 ?>
 <title>proIFPE::Home</title>
-<h3>Bem vindo, <?= $_SESSION['user_name']?>!</h3>
+<!-- <h3>Bem vindo, <?= $_SESSION['user_name']?>!</h3> -->
 <br>
 <?php if ($_SESSION['type_id'] == 1): ?>
-	<h2><a href="cad_turmas.php">Cadastrar turmas</a></h2>
-	<h2><a href="cad_alunos.php">Cadastrar alunos</a></h2>
-	<h2><a href="cad_professores.php">Cadastrar professores</a></h2>
-	<h2><a href="cad_disciplinas.php">Cadastrar disciplinas</a></h2>
-	<h2><a href="cad_administradores.php">Cadastrar administradores</a></h2>
-	<h2><a href="my_data.php">Perfil</a></h2>
-<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
-<?php endif ?>
-<?php if ($_SESSION['type_id'] == 2): ?>
-	<!-- <h2><a href="">Suas turmas</a></h2> -->
-	<h2><a href="my_classes_prof.php">Suas aulas</a></h2>
-	<!-- <h2><a href="notificacao.php">Ver notificações</a></h2> -->
-	<h2><a href="my_data.php">Perfil</a></h2>
-<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
-<?php endif ?>
-<?php if ($_SESSION['type_id'] == 3): ?>
-		<!-- <h2><a href="">Sua turma</a></h2> -->
-		<h2><a href="aln_classes">Suas aulas</a></h2>
-		<h2><a href="">Boletim</a></h2>
-		<!-- <h2><a href="notificacao.php">Ver notificações</a></h2> -->
-		<h2><a href="my_data.php">Perfil</a></h2>
+	<div class="menu-home">
+		<h2>Operações</h2>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="form_tur.php">Cadastrar Turmas</a></h3>
+			</div>
+		</div>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="form_aln.php">Cadastrar Alunos</a></h3>
+			</div>
+		</div>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="form_prof.php">Cadastrar Professores</a></h3>
+			</div>
+		</div>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="form_dis.php">Cadastrar Disciplinas</a></h3>
+			</div>
+		</div>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="form_adm.php">Cadastrar Administradores</a></h3>
+			</div>
+		</div>
+		<!-- <br> -->
+		<h2>Pessoal</h2>
+		<!-- <br> -->
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="my_data.php">Perfil</a></h3>
+			</div>
+		</div>
+		<br>
+	</div>
+	<!-- <h2><a href="form_tur.php">Cadastrar turmas</a></h2>
+	<h2><a href="form_aln.php">Cadastrar alunos</a></h2>
+	<h2><a href="form_prof.php">Cadastrar professores</a></h2>
+	<h2><a href="form_dis.php">Cadastrar disciplinas</a></h2>
+	<h2><a href="form_adm.php">Cadastrar administradores</a></h2>
+	<h2><a href="my_data.php">Perfil</a></h2> -->
 	<!-- 	<h3><a href="logout.php">Sair</a></h3> -->
 <?php endif ?>
-<br>
-<?php include 'footer.php'; ?>
+<?php if ($_SESSION['type_id'] == 2): ?>
+	<div class="menu-home">
+		<h2>Operações</h2>
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="my_classes_prof.php">Suas Aulas</a></h3>
+			</div>
+		</div>
+		<br>
+		<h2>Pessoal</h2>
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="my_data.php">Perfil</a></h3>
+			</div>
+		</div>
+	</div>
+	<!-- <h2><a href="">Suas turmas</a></h2>
+	<h2><a href="my_classes_prof.php">Suas aulas</a></h2>
+	<h2><a href="notificacao.php">Ver notificações</a></h2>
+	<h2><a href="my_data.php">Perfil</a></h2>
+	<h3><a href="logout.php">Sair</a></h3> -->
+<?php endif ?>
+<?php if ($_SESSION['type_id'] == 3): ?>
+	<div class="menu-home">
+		<h2>Operações</h2>
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="my_classes_prof.php">Suas Aulas</a></h3>
+			</div>
+		</div>
+		<br>
+		<h2>Pessoal</h2>
+		<div class="card-home">
+			<div class="content-card">
+				<h3><a href="my_data.php">Perfil</a></h3>
+			</div>
+		</div>
+	</div>
+		<!-- <h2><a href="">Sua turma</a></h2>
+		<h2><a href="my_classes_prof.php">Suas aulas</a></h2>
+		<h2><a href="">Boletim</a></h2>
+		<h2><a href="notificacao.php">Ver notificações</a></h2>
+		<h2><a href="my_data.php">Perfil</a></h2>
+		<h3><a href="logout.php">Sair</a></h3> -->
+	<?php endif ?>
+	<br>
+	<?php include 'footer.php'; ?>
